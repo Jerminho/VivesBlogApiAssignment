b44eb7edab882564af4705791bad7a8829b8bd28
Pierre Njiné Happy <pierrejeremieryan.njinehappy@student.vives.be>
2024-10-08 10:41:55 +0200

    Added Razor Runtime Compilation

--- VivesBlog.Ui.Mvc/Program.cs
+++ VivesBlog.Ui.Mvc/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<VivesBlogDbContext>(options =>
 {
     options.UseInMemoryDatabase(nameof(VivesBlogDbContext));
