726b83a15903691d44d58939a039cff2e500fe09
Pierre Njiné Happy <pierrejeremieryan.njinehappy@student.vives.be>
2024-10-08 14:01:55 +0200

    ArticleService updated and now uses DTO, ProjectionExtension updated.

--- VivesBlog.Services/ArticleService.cs
+++ VivesBlog.Services/ArticleService.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using VivesBlog.Core;
+using VivesBlog.Dto.Requests;
+using VivesBlog.Dto.Results;
 using VivesBlog.Model;
+using VivesBlog.Services.Extensions.Projection;
 
 namespace VivesBlog.Services
 {
@@ -14,65 +17,101 @@ namespace VivesBlog.Services
         }
 
         //Find
-        public IList<Article> Find()
+        public async Task<IList<ArticleResult>> Find()
         {
-            return _dbContext.Articles
-                .Include(a => a.Author)
-                .ToList();
+            return await _dbContext.Articles
+                .Project()
+                .ToListAsync();
         }
 
         //Get (by id)
-        public Article? Get(int id)
+        public async Task<ServiceResult<ArticleResult>> Get(int id)
         {
-            return _dbContext.Articles
-                .FirstOrDefault(p => p.Id == id);
-        }
+            var serviceResult = new ServiceResult<ArticleResult>();
 
-        //Create
-        public Article? Create(Article article)
-        {
-            article.PublishedDate = DateTime.UtcNow;
+            var article = await _dbContext.Articles
+                .Project()
+                .FirstOrDefaultAsync(a => a.Id == id);
 
-            _dbContext.Articles.Add(article);
-            _dbContext.SaveChanges();
-
-            return article;
-        }
-
-        //Update
-        public Article? Update(int id, Article article)
-        {
-            var dbArticle = _dbContext.Articles
-                .FirstOrDefault(p => p.Id == id);
-
-            if (dbArticle is null)
-            {
-                return null;
-            }
-
-            dbArticle.Title = article.Title;
-            dbArticle.Description = article.Description;
-            dbArticle.Content = article.Content;
-            dbArticle.AuthorId = article.AuthorId;
-
-            _dbContext.SaveChanges();
-
-            return dbArticle;
-        }
-
-        //Delete
-        public void Delete(int id)
-        {
-            var article = _dbContext.Articles
-                .FirstOrDefault(p => p.Id == id);
+            serviceResult.Data = article;
 
             if (article is null)
             {
-                return;
+                serviceResult.NotFound(nameof(Article), id);
+            }
+
+            return serviceResult;
+        }
+
+        //Create
+        public async Task<ServiceResult<ArticleResult>> Create(ArticleRequest request)
+        {
+            var serviceResult = new ServiceResult<ArticleResult>();
+
+            serviceResult.Validate(request);
+            if (!serviceResult.IsSuccess)
+            {
+                return serviceResult; 
+            }
+
+            var article = new Article
+            {
+                Title = request.Title,
+                Content = request.Content,
+                Description = request.Description,
+                AuthorId = request.AuthorId,
+                PublishedDate = DateTime.UtcNow
+            };
+
+            _dbContext.Articles.Add(article);
+            await _dbContext.SaveChangesAsync();
+
+            return await Get(article.Id);  
+        }
+
+        //Update
+        public async Task<ServiceResult<ArticleResult>> Update(int id, ArticleRequest request)
+        {
+            var serviceResult = new ServiceResult<ArticleResult>();
+
+            var article = await _dbContext.Articles
+                .FirstOrDefaultAsync(a => a.Id == id);
+
+            if (article is null)
+            {
+                serviceResult.NotFound(nameof(Article), id);
+                return serviceResult;
+            }
+
+            article.Title = request.Title;
+            article.Content = request.Content;
+            article.Description = request.Description;
+            article.AuthorId = request.AuthorId;
+
+            await _dbContext.SaveChangesAsync();
+
+            return await Get(id);
+        }
+
+        //Delete
+        public async Task<ServiceResult> Delete(int id)
+        {
+            var serviceResult = new ServiceResult();
+
+            var article = await _dbContext.Articles
+                .FirstOrDefaultAsync(a => a.Id == id);
+
+            if (article is null)
+            {
+                serviceResult.NotFound(nameof(Article), id);
+                return serviceResult;
             }
 
             _dbContext.Articles.Remove(article);
-            _dbContext.SaveChanges();
+            await _dbContext.SaveChangesAsync();
+
+            serviceResult.Deleted(nameof(Article));
+            return serviceResult;
         }
 
     }
--- VivesBlog.Services/Extensions/Projection/ProjectionExtensions.cs
+++ VivesBlog.Services/Extensions/Projection/ProjectionExtensions.cs
@@ -10,7 +10,22 @@ namespace VivesBlog.Services.Extensions.Projection
 {
     public static class PersonProjectionExtensions
     {
-        
+
+        public static IQueryable<ArticleResult> Project(this IQueryable<Article> query)
+        {
+            return query
+                .Select(a => new ArticleResult
+                {
+                    Id = a.Id,
+                    Title = a.Title,
+                    Description = a.Description,
+                    Content = a.Content,
+                    PublishedDate = a.PublishedDate,
+                    AuthorId = a.AuthorId,
+                    AuthorName = a.Author != null ? a.Author.FirstName + " " + a.Author.LastName : null
+                });
+        }
+
         public static IQueryable<PersonResult> Project(this IQueryable<Person> query)
         {
             return query.Select(p => new PersonResult
